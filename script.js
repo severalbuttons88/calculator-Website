9d46f7496a057d10daea14d7de001ec03abb99f3
Severalbuttons88 <44512019+severalbuttons88@users.noreply.github.com>
2023-01-05 17:07:19 -0600

    Add 0 as default display value

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const inputButton = document.querySelectorAll(".num-input");
 const interfaceSelector = document.querySelectorAll(".interface");
 const equalsButton = document.querySelector(".equals");
 const clearButton = document.querySelector(".clear");
+displayOutput.textContent = "0";
 let userInputValue1 = "";
 let userInputValue2 = "";
 let threeInputToggle = 0;
@@ -38,10 +39,10 @@ function operate(operator, num1, num2) {
 function userInput() {
   inputButton.forEach((button) => {
     button.addEventListener("click", (e) => {
-        if(threeInputToggle === 2) {
-            displayOutput.textContent = "";
-            threeInputToggle = 0;
-        }
+      if (threeInputToggle === 2) {
+        displayOutput.textContent = "";
+        threeInputToggle = 0;
+      }
       if (threeInputToggle === 0) {
         let num = e.target.id;
         userInputValue1 += num;
@@ -64,30 +65,28 @@ function userInterfaceEvent() {
   });
 }
 function equalsInput() {
-    equalsButton.addEventListener("click", () => {
-        displayOutput.textContent = ""
-        let evaluatedValue = operate(operatorInput, Number(userInputValue1), Number(userInputValue2));
-        displayOutput.textContent = evaluatedValue.toString();
-        userInputValue1 = "";
-        userInputValue2 = "";
-        threeInputToggle = 2;
-
-    });
-
-
+  equalsButton.addEventListener("click", () => {
+    displayOutput.textContent = "0";
+    let evaluatedValue = operate(
+      operatorInput,
+      Number(userInputValue1),
+      Number(userInputValue2)
+    );
+    displayOutput.textContent = evaluatedValue.toString();
+    userInputValue1 = "";
+    userInputValue2 = "";
+    threeInputToggle = 2;
+  });
 }
 function clearDisplay() {
-    clearButton.addEventListener("click", () => {
-        displayOutput.textContent = "";
-        userInputValue1 = "";
-        userInputValue2 = "";
-        threeInputToggle = 0;
-    });
-
+  clearButton.addEventListener("click", () => {
+    displayOutput.textContent = "0";
+    userInputValue1 = "";
+    userInputValue2 = "";
+    threeInputToggle = 0;
+  });
 }
 userInput();
 userInterfaceEvent();
 equalsInput();
 clearDisplay();
-
-
