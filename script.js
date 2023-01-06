be1c2186952240a4b1c0f34e58ebe90887e19dcc
Severalbuttons88 <44512019+severalbuttons88@users.noreply.github.com>
2023-01-05 18:32:39 -0600

    Fix bugs and clean up interface inputs

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const clearButton = document.querySelector(".clear");
 displayOutput.textContent = "0";
 let userInputValue1 = "";
 let userInputValue2 = "";
+let runningTotal = 0;
 let threeInputToggle = 0;
 let operatorInput = 0;
 function add(num1, num2) {
@@ -40,17 +41,43 @@ function userInput() {
   inputButton.forEach((button) => {
     button.addEventListener("click", (e) => {
       if (threeInputToggle === 2) {
-        displayOutput.textContent = "";
-        threeInputToggle = 0;
+        let num = e.target.id;
+        if (userInputValue1 === "0" && num === "0") {
+            userInputValue1 = "0";
+          } else if (userInputValue1 === "0" && num !== "0") {
+            userInputValue1 = "";
+            userInputValue1 += num;
+            displayOutput.textContent = userInputValue1;
+          } else {
+            userInputValue1 += num;
+            displayOutput.textContent = userInputValue1;
+          }
+
       }
       if (threeInputToggle === 0) {
         let num = e.target.id;
-        userInputValue1 += num;
-        displayOutput.textContent = userInputValue1;
+        if (userInputValue1 === "0" && num === "0") {
+          userInputValue1 = "0";
+        } else if (userInputValue1 === "0" && num !== "0") {
+          userInputValue1 = "";
+          userInputValue1 += num;
+          displayOutput.textContent = userInputValue1;
+        } else {
+          userInputValue1 += num;
+          displayOutput.textContent = userInputValue1;
+        }
       } else if (threeInputToggle === 1) {
         let num = e.target.id;
-        userInputValue2 += num;
-        displayOutput.textContent = userInputValue2;
+        if (userInputValue2 === "0" && num === "0") {
+            userInputValue2 = "0";
+          } else if (userInputValue2 === "0" && num !== "0") {
+            userInputValue2 = "";
+            userInputValue2 += num;
+            displayOutput.textContent = userInputValue2;
+          } else {
+            userInputValue2 += num;
+            displayOutput.textContent = userInputValue2;
+          }
       }
     });
   });
@@ -58,29 +85,47 @@ function userInput() {
 function userInterfaceEvent() {
   interfaceSelector.forEach((interface) => {
     interface.addEventListener("click", (e) => {
-      operatorInput = Number(e.target.id);
-      displayOutput.textContent = "";
-      threeInputToggle = 1;
+      if (threeInputToggle === 2) {
+        operatorInput = Number(e.target.id);
+      } else if (threeInputToggle !== 2) {
+        operatorInput = Number(e.target.id);
+
+        threeInputToggle = 1;
+      }
     });
   });
 }
 function equalsInput() {
   equalsButton.addEventListener("click", () => {
-    displayOutput.textContent = "0";
-    let evaluatedValue = operate(
-      operatorInput,
-      Number(userInputValue1),
-      Number(userInputValue2)
-    );
-    displayOutput.textContent = evaluatedValue.toString();
-    userInputValue1 = "";
-    userInputValue2 = "";
-    threeInputToggle = 2;
+    if (threeInputToggle === 2) {
+      runningTotal =
+        Math.round(
+          operate(operatorInput, runningTotal, Number(userInputValue1)) * 1000
+        ) / 1000;
+      displayOutput.textContent = runningTotal.toString();
+      operatorInput = 0;
+      userInputValue1 = "";
+    } else if (threeInputToggle === 0 || threeInputToggle === 1) {
+      runningTotal =
+        Math.round(
+          operate(
+            operatorInput,
+            Number(userInputValue1),
+            Number(userInputValue2)
+          ) * 1000
+        ) / 1000;
+      displayOutput.textContent = runningTotal.toString();
+      operatorInput = 0;
+      userInputValue1 = "";
+      userInputValue2 = "";
+      threeInputToggle = 2;
+    }
   });
 }
 function clearDisplay() {
   clearButton.addEventListener("click", () => {
     displayOutput.textContent = "0";
+    runningTotal = 0;
     userInputValue1 = "";
     userInputValue2 = "";
     threeInputToggle = 0;
